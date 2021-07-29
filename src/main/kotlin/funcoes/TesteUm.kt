98a70a664d0f2b5211178e60e565c5e65fe4822a
Judson Paiva <judson.paiva@hotmail.com>
2021-07-29 20:24:16 +0100

    üsando o args da função main

--- src/main/kotlin/funcoes/TesteUm.kt
+++ src/main/kotlin/funcoes/TesteUm.kt
@@ -2,5 +2,14 @@ package funcoes
 
 
 fun main(args: Array<String>) {
-    println("Olá ${args[1]}")
+    //println("Olá ${args[1]}")
+
+    val isUnit = println("This is an expression")
+    println(isUnit)
+
+    var retorno = verificar("11")
+    println(retorno)
 }
+
+
+fun verificar(str: String): String = if (str.toInt() < 12) "Good morning, Kotlin" else "Good night, Kotlin"
