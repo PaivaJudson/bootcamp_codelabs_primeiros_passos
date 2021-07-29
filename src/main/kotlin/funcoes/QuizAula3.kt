7eef062a9d8ae5c6c20c0fd3f4ad60277ebc3143
Judson Paiva <judson.paiva@hotmail.com>
2021-07-29 23:00:32 +0100

    adicionando funções compactas, filtros e lambdas em kotlin

--- src/main/kotlin/funcoes/QuizAula3.kt
+++ src/main/kotlin/funcoes/QuizAula3.kt
@@ -3,7 +3,30 @@ package funcoes
 fun main() {
 
     //println("Your fortune is: ${getFortuneCookie()}")
+    val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
 
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
+
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+
+    /*
+    println( decorations.filter {it[1] == 'l'})
+    val lista1 = decorations.filter {it[1] == 'l'}
+
+    println("Oficial = $lista1")
+    val lista2 = decorations.filter { it [0] == 'p' }
+    println("lista2 = ${lista2}")
+
+    val lista3 = decorations.asSequence().filter { it [0] == 'p' }.toList()
+    println("lista3 = ${lista3}")
+    */
 }
 
 fun getFortuneCookie(): String{
@@ -19,3 +42,17 @@ fun getFortuneCookie(): String{
 
 
 
+
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
+
+fun swim(speed: String = "fast") = println("swimming ${speed}")
+
+fun f1(x: Int, y: Int = 2, z: Int = funcao()): Boolean{
+    return true
+}
+
+fun funcao(): Int = 2
